802a3e530356831d8e2c0d9d11585e43c50b5785
Daniel JB Clark <djbclark@gmail.com>
2026-09-21 11:40:07 -0400

    aiuse 3.0.37

--- Formula/aiuse.rb
+++ Formula/aiuse.rb
@@ -3,8 +3,8 @@ class Aiuse < Formula
 
   desc "Aggregate AI subscription quotas and flag use-it-or-lose-it allotments"
   homepage "https://github.com/djbclark/aiuse"
-  url "https://github.com/djbclark/aiuse/archive/refs/tags/v3.0.36.tar.gz"
-  sha256 "25e307642511d2ec837a00ca123d670b8fc25f0e4779bfaa36a4290b25e3a8eb"
+  url "https://github.com/djbclark/aiuse/archive/refs/tags/v3.0.37.tar.gz"
+  sha256 "783f29f62dd95d275dbf619504a15500df3eb65f5f51c4cd112c45d18a6694d9"
   license "MIT"
   head "https://github.com/djbclark/aiuse.git", branch: "main"
 
